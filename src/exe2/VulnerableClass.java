6a03eb8c50c0882188ae9e8e91b600fd02b39a62
Gabriel Brito <gabriel.brito55@gmail.com>
2017-07-01 18:33:59 -0300

    testes com 98% de cobertura

--- src/exe2/VulnerableClass.java
+++ src/exe2/VulnerableClass.java
@@ -6,57 +6,92 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VulnerableClass {
-	public void vulnerableMethod(String FILENAME){
-		while (true) {
-		    Scanner console = new Scanner(System.in);
-		    System.out.print("Digite a operacao desejada para realizar no arquivo <R para ler um arquivo, "
-		    		+ "W para escrever em um arquivo>? ");
+	//O metodo sera declarado boolean a fim de facilitar a implementacao dos testes
+	public boolean vulnerableMethod(String FILENAME){
+		
+		//verficacao string com o nome do arquivo
+		if(!isValidFile(FILENAME)){
+			return false;
+		}
+		
+	    Scanner console = new Scanner(System.in);
+	    System.out.print("Digite a operacao desejada para realizar no arquivo <R para ler um arquivo, "
+	    		+ "W para escrever em um arquivo>? ");
+		
+	    String opr= console.nextLine();
+		
+	    if (opr.equals("R")){
+			BufferedReader br = null;
+			FileReader fr = null;
 			
-		    String opr= console.nextLine();
-			
-		    if (opr.equals("R")){
-				BufferedReader br = null;
-				FileReader fr = null;
-				
-				try {
+			try {
 
-					fr = new FileReader(FILENAME);
-					br = new BufferedReader(fr);
+				fr = new FileReader(FILENAME);
+				br = new BufferedReader(fr);
 
-					String sCurrentLine;
+				String sCurrentLine;
 
-					br = new BufferedReader(new FileReader(FILENAME));
+				br = new BufferedReader(new FileReader(FILENAME));
 
-					while ((sCurrentLine = br.readLine()) != null) {
-						System.out.println(sCurrentLine);
-					}
+				while ((sCurrentLine = br.readLine()) != null) {
+					System.out.println(sCurrentLine);
+				}
 
-				} catch (IOException e) {
+			} catch (IOException e) {
 
-					e.printStackTrace();
+				e.printStackTrace();
 
-				} 
-			}
-			
-			else {
-				  BufferedWriter buffWrite;
-				  
-				  try {
-					buffWrite = new BufferedWriter(new FileWriter(FILENAME));
-					String linha = "";
-					System.out.println("Escreva algo: ");
-				    linha = console.nextLine();
-				    buffWrite.append(linha + "\n");
-				    
-				    //Buffer precisa ser fechado para efetuar as alterações
-				    buffWrite.close();
-				     
-				} catch (IOException e) {
-					e.printStackTrace();
-				} 
+			} 
+		}
+		
+	    if (opr.equals("W")){
+			  BufferedWriter buffWrite;
+			  
+			  try {
+				buffWrite = new BufferedWriter(new FileWriter(FILENAME));
+				String linha = "";
+				System.out.println("Escreva algo: ");
+			    linha = console.nextLine();
+			    buffWrite.append(linha + "\n");
+			    
+			    //Buffer precisa ser fechado para efetuar as alterações
+			    buffWrite.close();
+			     
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
+		return true;
+	}
+	
+	private boolean isValidFile(String file){
+		String[] folders = file.split("\\\\");
+		for(int i = 0; i < folders.length; i++){
+			if(folders[i].equals(".."))
+				return false;
+			if(folders[i].equals("."))
+				return false;
+		}
+		
+		//Verificando nome de arquivo
+		String filename = folders[folders.length-1];
+		String[] aux = filename.split("\\.");
+		String name = aux[0];
+		String extension = aux[aux.length-1];
+		
+		Pattern pattern = Pattern.compile("[^A-Za-z0-9._]");
+		Matcher matcher = pattern.matcher(name);
+		if(matcher.find()){
+			return false;
+		}
+		
+		if(!extension.equals("txt"))
+			return false;
+		
+		return true;
 	}
 }
